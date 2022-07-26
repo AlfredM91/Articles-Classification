7e5f7a530edd01b365dad3336f664e64f287494f
AlfredM91 <107662349+AlfredM91@users.noreply.github.com>
2022-07-26 10:39:14 +0800

    Submission

--- articles_nlp.py
+++ articles_nlp.py
@@ -20,8 +20,8 @@ df.head()
 df.duplicated().sum()
 df.isna().sum()
 
-print(df['review'][4])
-print(df['review'][10])
+print(df['text'][4])
+print(df['text'][10])
 
 # Symbols and HTML Tags have to be removed
 
@@ -29,18 +29,195 @@ print(df['review'][10])
 
 import re
 
-review = df['review']
-sentiment = df['sentiment']
+text = df['text']
+category = df['category']
 
 
-for index, text in enumerate(df['review']):
+for index, words in enumerate(df['text']):
     # to remove html tags
     # anything within the <> will be removed including <>
     # ? to tell re don't be greedy so it won't capture everything
     # from the first < to the last > in the document
     
-    review[index] = re.sub('<.*?>','',text)
-    review[index] = re.sub('[^a-zA-Z]',' ',text).lower().split()    
+    text[index] = re.sub('<.*?>','',words)   
+    text[index] = re.sub('[^a-zA-Z]',' ',words).lower().split()    
 
-review_backup = review.copy()
-sentiment_backup = sentiment.copy()
+text_backup = text.copy()
+category_backup = category.copy()
+
+#%% Step 5) Data Preprocessing
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+import numpy as np
+
+vocab_size = 0
+compiled = []
+
+
+for i in np.arange(0,df.shape[0],1):
+    compiled.extend(text[i])
+
+vocab_size = len(set(compiled))
+
+oov_token = '<OOV>'
+
+tokenizer = Tokenizer(num_words=vocab_size,oov_token=oov_token)
+tokenizer.fit_on_texts(text)
+
+# word_index = tokenizer.word_index
+
+# print(dict(list(word_index.items())[0:10]))
+
+text_int = tokenizer.texts_to_sequences(text) # to convert into numbers
+
+length_text = []
+
+for i in range(len(text_int)):
+    length_text.append(len(text_int[i]))
+    # print(len(review_int[i]))
+
+import numpy as np
+
+max_len = np.median([len(text_int[i]) for i in range(len(text_int))])
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+padded_text = pad_sequences(text_int,
+                              maxlen=int(max_len),
+                              padding='post',
+                              truncating='post')
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+
+# y target
+
+ohe = OneHotEncoder(sparse=False)
+category = ohe.fit_transform(np.expand_dims(category,axis=-1))
+
+X_train, X_test, y_train, y_test = train_test_split(padded_text,category,
+                                                    test_size=0.3,
+                                                    random_state=123)
+
+#%% Model Development
+
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Embedding, Bidirectional
+from tensorflow.keras import Input, Sequential
+
+input_shape = np.shape(X_train)[1:]
+
+model = Sequential()
+model.add(Input(shape=(input_shape)))
+model.add(Embedding(vocab_size,128)) # put embedding right after input
+model.add(Bidirectional(LSTM(128,return_sequences=True))) # return sequence is true if passing to rnn,gru,ltsm
+model.add(Dropout(0.3))
+model.add(Bidirectional(LSTM(128)))
+model.add(Dropout(0.3))
+model.add(Dense(5,activation='softmax'))
+
+model.summary()
+
+from tensorflow.keras.utils import plot_model
+
+PLOT_PATH = os.path.join(os.getcwd(),'statics','model.png')
+
+plot_model(model,show_shapes=True,to_file=PLOT_PATH)
+
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['acc'])
+
+#%%
+
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
+import datetime
+
+LOGS_PATH = os.path.join(os.getcwd(),'logs',datetime.datetime.now().
+                         strftime('%Y%m%d-%H%M%S'))
+
+tensorboard_callback = TensorBoard(log_dir=LOGS_PATH, histogram_freq=1)
+early_callback = EarlyStopping(monitor='val_loss',patience=5)
+
+hist = model.fit(X_train,y_train,
+          validation_data=(X_test,y_test),
+          epochs=5,
+          callbacks=[tensorboard_callback])
+
+# open anaconda prompt
+# type cd "folder path to your logs file"
+# type tensorboard --logdir logs
+
+# in colab, type %load_ext tensorboard
+# type %tensorboard --logdir (logs folder name that you uploaded)
+
+#%% Model Evaluation/Analysis
+
+import matplotlib.pyplot as plt
+keys = list(hist.history.keys())
+plt.figure()
+plt.plot(hist.history[keys[0]])
+plt.plot(hist.history[keys[2]])
+plt.xlabel('Epoch')
+plt.legend(['Training '+keys[0],'Validation '+keys[0]])
+plt.show()
+
+plt.figure()
+plt.plot(hist.history[keys[1]])
+plt.plot(hist.history[keys[3]])
+plt.xlabel('Epoch')
+plt.legend(['Training '+keys[1],'Validation '+keys[1]])
+plt.show()
+
+from sklearn.metrics import classification_report
+
+y_pred = np.argmax(model.predict(X_test),axis=1)
+y_actual = np.argmax(y_test,axis=1)
+
+print(classification_report(y_actual, y_pred))
+
+#%% Model Saving
+
+# Tokenizer
+
+import json
+TOKENIZER_SAVE_PATH = os.path.join(os.getcwd(),'saved_models','tokenizer.json')
+
+token_json = tokenizer.to_json()
+
+with open(TOKENIZER_SAVE_PATH,'w') as file:
+    json.dump(token_json,file)
+
+
+# OHE
+
+import pickle
+
+OHE_SAVE_PATH = os.path.join(os.getcwd(),'saved_models','ohe.pkl')
+
+with open(OHE_SAVE_PATH,'wb') as file:
+    pickle.dump(ohe,file)
+
+# MODEL
+
+MODEL_SAVE_PATH = os.path.join(os.getcwd(),'saved_models','model.h5')
+model.save(MODEL_SAVE_PATH)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
